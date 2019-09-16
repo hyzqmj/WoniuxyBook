8122f7cadf3223103fd7f38f6e36ae9dc63aec40
王沛 <@LAPTOP-GJ29582U.lan>
2019-09-16 14:41:32 +0800

    角色表的service接口及service

---
+++ src/main/java/com/woniuxy/service/impl/RolesService.java
@@ -0,0 +1,40 @@
+package com.woniuxy.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.woniuxy.dao.RolesMapper;
+import com.woniuxy.domain.Roles;
+import com.woniuxy.service.IRolesService;
+@Service
+public class RolesService implements IRolesService {
+	@Autowired
+	private RolesMapper rolesMapper;
+	@Override
+	public void save(Roles role) {
+		rolesMapper.insertSelective(role);
+	}
+
+	@Override
+	public void delete(Integer rolesid) {
+		rolesMapper.deleteByPrimaryKey(rolesid);
+	}
+
+	@Override
+	public void update(Roles role) {
+		rolesMapper.updateByPrimaryKey(role);
+	}
+
+	@Override
+	public List<Roles> findAll() {
+		return rolesMapper.selectByExample(null);
+	}
+
+	@Override
+	public Roles findOne(Integer rolesid) {
+		return rolesMapper.selectByPrimaryKey(rolesid);
+	}
+
+}
