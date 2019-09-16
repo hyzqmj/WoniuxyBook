b02af0ae9d2d949303a924159cac2f520cc4aa39
王沛 <@LAPTOP-GJ29582U.lan>
2019-09-16 15:57:42 +0800

    角色表增加了@Transactional注解，增加了权限表service接口及service

--- src/main/java/com/woniuxy/service/impl/RolesService.java
+++ src/main/java/com/woniuxy/service/impl/RolesService.java
@@ -4,37 +4,39 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.woniuxy.dao.RolesMapper;
 import com.woniuxy.domain.Roles;
 import com.woniuxy.service.IRolesService;
 @Service
+@Transactional
 public class RolesService implements IRolesService {
 	@Autowired
-	private RolesMapper rolesMapper;
+	private RolesMapper mapper;
 	@Override
 	public void save(Roles role) {
-		rolesMapper.insertSelective(role);
+		mapper.insertSelective(role);
 	}
 
 	@Override
 	public void delete(Integer rolesid) {   
-		rolesMapper.deleteByPrimaryKey(rolesid); 
+		mapper.deleteByPrimaryKey(rolesid);
 	}
 
 	@Override
 	public void update(Roles role) {
-		rolesMapper.updateByPrimaryKey(role);
+		mapper.updateByPrimaryKey(role);
 	}
 
 	@Override
 	public List<Roles> findAll() {
-		return rolesMapper.selectByExample(null);
+		return mapper.selectByExample(null);
 	}
 
 	@Override
 	public Roles findOne(Integer rolesid) {
-		return rolesMapper.selectByPrimaryKey(rolesid);
+		return mapper.selectByPrimaryKey(rolesid);
 	}
 
 }
