cb0e644ff8f85d33d502d80ee40f756e75ca5615
peng <850856288@qq.com>
2019-09-17 15:51:38 +0800

    添加扫包，添加配置文件的数据库连接

--- src/main/java/com/woniuxy/WoniuxyBookApplication.java
+++ src/main/java/com/woniuxy/WoniuxyBookApplication.java
@@ -1,9 +1,11 @@
 package com.woniuxy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.woniuxy")
 public class WoniuxyBookApplication {
 
 	public static void main(String[] args) {
